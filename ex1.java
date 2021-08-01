142c826b6ff8c1a9966a37e9f2c8f03cfd78dbe6
Catalin Petris <petriscatalin@gmail.com>
2021-08-01 15:19:44 +0300

    third

--- ex1.java
+++ ex1.java
@@ -1,7 +1,10 @@
 class Test{
 
+public static void main(String[] args){
+
   System.out.println("Hello");
   System.out.println("S-a modificat");
   System.out.println("S-a modificat din nou");
 
+}
 }
